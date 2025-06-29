688a8468edcf220a6caefe8813ae5ba4ba882e9d
lazcansar <abdullahgoksal@outlook.com>
2025-06-29 17:30:52 +0300

    Login Page Design

--- login.php
+++ login.php
@@ -4,13 +4,21 @@ require_once 'header.php';
 
 <section class="px-4 sm:px-0">
     <div class="container mx-auto">
-        <div class="border p-4 rounded shadow">
-            <h1>Giriş Yap</h1>
-            <form action="" method="POST">
-                <input type="text" name="email" class="" placeholder="E-Mail">
-                <input type="password" name="password" class="" placeholder="*******">
-            </form>
+        <div class="flex flex-row items-center justify-center">
+            <div class="w-[540px]">
+                <div class="border p-4 rounded shadow bg-white">
+                    <h1 class="mb-4 font-medium text-xl text-teal-900">Giriş Yap</h1>
+                    <form action="" method="POST">
+                        <label class="text-gray-800 my-2 block" for="email">E-Posta Adresi</label>
+                        <input type="text" name="email" class="w-full rounded border px-2 py-1 outline-none mb-4" id="email" placeholder="E-Mail">
+                        <label class="text-gray-800 my-2 block" for="password">Şifre</label>
+                        <input type="password" name="password" class="w-full rounded border px-2 py-1 outline-none mb-4" id="password" placeholder="*******">
+                        <button type="submit" class="inline-block bg-blue-600 hover:bg-blue-500 text-white transition px-4 py-2 rounded-lg">Giriş Yap</button>
+                    </form>
+                </div>
+            </div>
         </div>
+
 
     </div>
 </section>
