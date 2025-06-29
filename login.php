da947203a954f9ddf0cdc3b61721971e0157d649
lazcansar <abdullahgoksal@outlook.com>
2025-06-29 17:36:48 +0300

    Login Page Design

--- login.php
+++ login.php
@@ -7,13 +7,16 @@ require_once 'header.php';
         <div class="flex flex-row items-center justify-center">
             <div class="w-[540px]">
                 <div class="border p-4 rounded shadow bg-white">
-                    <h1 class="mb-4 font-medium text-xl text-teal-900">Giriş Yap</h1>
+                    <h1 class="mb-4 font-medium text-xl text-teal-900">Giriş Ekranı</h1>
                     <form action="" method="POST">
                         <label class="text-gray-800 my-2 block" for="email">E-Posta Adresi</label>
                         <input type="text" name="email" class="w-full rounded border px-2 py-1 outline-none mb-4" id="email" placeholder="E-Mail">
-                        <label class="text-gray-800 my-2 block" for="password">Şifre</label>
+                        <label class="text-gray-800 mb-2 block" for="password">Şifre</label>
                         <input type="password" name="password" class="w-full rounded border px-2 py-1 outline-none mb-4" id="password" placeholder="*******">
-                        <button type="submit" class="inline-block bg-blue-600 hover:bg-blue-500 text-white transition px-4 py-2 rounded-lg">Giriş Yap</button>
+                        <div class="flex flex-row flex-wrap items-center justify-between">
+                            <button type="submit" class="inline-block bg-blue-600 hover:bg-blue-500 text-white transition px-4 py-2 rounded-lg">Giriş Yap</button>
+                            <a href="register.php" class="inline-block bg-amber-600 hover:bg-amber-500 text-white transition px-4 py-2 rounded-lg">Hesabınız mı yok? Hemen kayıt ol!</a>
+                        </div>
                     </form>
                 </div>
             </div>
