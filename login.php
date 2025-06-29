7762017b93ed34cdc1205805a1787a715f44d64e
lazcansar <abdullahgoksal@outlook.com>
2025-06-29 18:35:54 +0300

    Operations file created. Request operations

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'header.php';
+require_once 'component.php';
 ?>
 
 <section class="px-4 sm:px-0">
@@ -8,13 +9,13 @@ require_once 'header.php';
             <div class="w-[540px]">
                 <div class="border p-4 rounded shadow bg-white">
                     <h1 class="mb-4 font-medium text-xl text-teal-900">Giriş Ekranı</h1>
-                    <form action="" method="POST">
-                        <label class="text-gray-800 my-2 block" for="email">E-Posta Adresi</label>
-                        <input type="text" name="email" class="w-full rounded border px-2 py-1 outline-none mb-4" id="email" placeholder="E-Mail">
-                        <label class="text-gray-800 mb-2 block" for="password">Şifre</label>
-                        <input type="password" name="password" class="w-full rounded border px-2 py-1 outline-none mb-4" id="password" placeholder="*******">
+                    <form action="operations.php" method="POST">
+                        <?php
+                        echo inputCompontent('email', 'email', 'E-mail Adresiniz');
+                        echo inputCompontent('password', 'password', 'Şifreniz');
+                        ?>
                         <div class="flex flex-row flex-wrap items-center justify-between">
-                            <button type="submit" class="inline-block bg-blue-600 hover:bg-blue-500 text-white transition px-4 py-2 rounded-lg">Giriş Yap</button>
+                            <button type="submit" name="login" class="inline-block bg-blue-600 hover:bg-blue-500 text-white transition px-4 py-2 rounded-lg">Giriş Yap</button>
                             <a href="register.php" class="inline-block bg-amber-600 hover:bg-amber-500 text-white transition px-4 py-2 rounded-lg">Hesabınız mı yok? Hemen kayıt ol!</a>
                         </div>
                     </form>
--- operations.php
+++ operations.php
@@ -21,5 +21,30 @@ if (isset($_POST['register'])) {
     $stmt->close();
 }
 
+// User Login Post Method
+if (isset($_POST['login'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $password = $_POST['password'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $db->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row['password'])) {
+        session_start();
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['phone'] = $row['phone'];
+        header("Location: index.php");
+        exit();
+    } else {
+        header("Location: register.php?login=fail");
+        exit();
+    }
+    $stmt->close();
+}
+
 
 ?>
